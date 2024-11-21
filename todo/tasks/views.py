a357123b27b3310a75f923e702009484be8d8d22
Симионов Андрей Владимирович <avsimionov@edu.hse.ru>
2024-11-21 03:09:37 +0300

    Add Searching and Ordering to view

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Task
 from rest_framework.pagination import PageNumberPagination
 from .serializers import TaskSerializer
@@ -15,8 +15,10 @@ class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     pagination_class = CustomPageNumberPagination
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ["status"]
+    ordering_fields = ["created_at", "status"]
+    search_fields = ["title", "description"]
 
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
