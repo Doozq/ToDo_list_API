2a63c26f5085e095b14dab36fcabc9ee9a614ece
Симионов Андрей Владимирович <avsimionov@edu.hse.ru>
2024-11-21 15:50:23 +0300

    Add file field to Task

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -11,5 +11,6 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
     status = models.CharField(max_length=9, choices=STATUS_CHOICES, default='NEW')
+    file = models.FileField(upload_to='task_files/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,5 +13,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/', include('tasks.urls')),
-]
+    path('api/tasks/', include('tasks.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
